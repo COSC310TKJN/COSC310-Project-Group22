c38086dc2913373e112fe7ea5da75248ca6179ca
TylerTheCamaroGuy <144307348+TylerTheCamaroGuy@users.noreply.github.com>
2026-03-15 22:02:27 -0700

    Added order_routes and order_schema

--- backend/tests/test_orders.py
+++ backend/tests/test_orders.py
@@ -1,6 +1,8 @@
 import unittest
+from pydantic import ValidationError
 from models.order import Order, OrderStatus
-
+from routes.order_routes import create_order
+from schemas.order_schema import OrderCreate
 
 
 class TestOrders(unittest.TestCase):
@@ -35,6 +37,39 @@ class TestOrders(unittest.TestCase):
                 customer_id="C2"
             )
  
+    def test_eligibility(self):
+
+        order = OrderCreate(
+            order_id="3",
+            restaurant_id=10,
+            food_item="Pizza",
+            order_time="2025-03-11T12:00:00",
+            order_value=25,
+            delivery_method="bike",
+            delivery_distance=5,
+            customer_id="C3"
+        )
+
+        response = create_order(order)
+
+        self.assertEqual(response["message"], "Order created")
+        self.assertIn("order", response)
+
+    def test_invalid_order_value_type(self):
+
+        with self.assertRaises(ValidationError):
+
+            OrderCreate(
+            order_id="4",
+            restaurant_id=5,
+            food_item="Burger",
+            order_time="2025-03-11T12:00:00",
+            order_value="twenty",
+            delivery_method="car",
+            delivery_distance=4,
+            customer_id="C4"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
