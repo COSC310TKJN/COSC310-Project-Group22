7cbaeede31abb85bc6e327c41398f47c581b6539
TylerTheCamaroGuy <144307348+TylerTheCamaroGuy@users.noreply.github.com>
2026-03-16 22:44:39 -0700

    order_service Modified and Testing (#50,#51)
    - Added cancel order def into the order service file
    - Added various tests which cover order to database
    - Added another test which ensures orders can't be canceled after creation.

--- backend/tests/test_orders.py
+++ backend/tests/test_orders.py
@@ -1,5 +1,9 @@
 import unittest
+from pydantic import ValidationError
 from models.order import Order, OrderStatus
+from routes.order_routes import create_order
+from schemas.order_schema import OrderCreate
+from services.order_service import OrderService, orders_db
 
 
 
@@ -34,7 +38,44 @@ class TestOrders(unittest.TestCase):
                 delivery_distance=4,
                 customer_id="C2"
             )
- 
+
+    def test_order_to_database(self):
+
+        order = OrderCreate(
+            order_id="3",
+            restaurant_id=10,
+            food_item="Pasta",
+            order_time="2025-03-11T12:00:00",
+            order_value=18,
+            delivery_method="car",
+            delivery_distance=3,
+            customer_id="C5"
+        )
+
+        created_order = OrderService.create_order(order)
+
+        self.assertIn("3", orders_db)
+        self.assertEqual(orders_db["3"], created_order)
+
+    def test_cancel_order(self):
+
+        order = OrderCreate(
+            order_id="4",
+            restaurant_id=10,
+            food_item="Sushi",
+            order_time="2025-03-11T12:00:00",
+            order_value=30,
+            delivery_method="bike",
+            delivery_distance=6,
+            customer_id="C4"
+        )
+
+        OrderService.create_order(order)
+
+        cancelled_order = OrderService.cancel_order("4")
+
+        self.assertEqual(cancelled_order.status, OrderStatus.CANCELLED)
+
 
 if __name__ == "__main__":
     unittest.main()
