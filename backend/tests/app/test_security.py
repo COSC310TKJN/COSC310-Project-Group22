fb1ff0ca1c4f82952758b835cbbff78dcecd22f3
Jason Lange <Jasonlange11@hotmail.com>
2026-03-23 15:55:34 -0700

    removed security tests as they're not in scope for issue 5

--- backend/tests/app/test_security.py
+++
@@ -1,8 +0,0 @@
-from backend.app.security import hash_password
-
-
-def test_hash_password_returns_sha256_string():
-    hashed_password = hash_password("StrongPass123")
-
-    assert hashed_password != "StrongPass123"
-    assert len(hashed_password) == 64
