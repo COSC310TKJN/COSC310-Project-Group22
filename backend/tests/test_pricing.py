3ff1d737646e3fb57a65dc4df7bf7961f9de0601
TylerTheCamaroGuy <144307348+TylerTheCamaroGuy@users.noreply.github.com>
2026-03-23 13:20:05 -0700

    Sub Order Calculation and Testing (Issues #69,74)
    Very simple PR
    
    - Sets a rate for the bike car as well as tax used later on
    - Then does a simple calculation of the fee associated with each service
    - Testing ensures each fee is correct

---
+++ backend/tests/test_pricing.py
@@ -0,0 +1,22 @@
+from services.pricing_service import PricingService
+
+
+def test_bike_delivery_fee():
+
+    fee = PricingService.calc_delivery_fee("bike", 5)
+
+    assert fee == 5
+
+
+def test_car_delivery_fee():
+
+    fee = PricingService.calc_delivery_fee("car", 4)
+
+    assert fee == 6
+
+
+def test_car_default_case():
+
+    fee = PricingService.calc_delivery_fee("car", 2)
+
+    assert fee == 3
