e464646669b7377d3612e9df516d3f8dbb1e579b
KaranJangra05 <144311756+KaranJangra05@users.noreply.github.com>
2026-03-25 20:01:18 -0700

    Update main.py
    includes delivery routes in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from backend.app.config import settings
 from backend.app.database import Base, engine
 from backend.app.routes import notification_routes, payment_routes, auth_routes, restaurant_routes
+from backend.routes import delivery_routes
 from backend.app.bootstrap import check_menu_items_exist, check_restaurants_exist
 
 Base.metadata.create_all(bind=engine)
@@ -23,6 +24,7 @@ app.include_router(payment_routes.router)
 app.include_router(auth_routes.router)
 app.include_router(notification_routes.router)
 app.include_router(restaurant_routes.router)
+app.include_router(delivery_routes.router)
 
 
 @app.get("/health")
